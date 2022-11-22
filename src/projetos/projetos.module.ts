d5c8bcd5b50e8918e2c0060d1fa17c52df15bd97
Patrick Delfim <patrickdelfim@gmail.com>
2022-11-21 21:58:54 -0300

    fix: fix projeto imports

--- src/projetos/projetos.module.ts
+++ src/projetos/projetos.module.ts
@@ -4,10 +4,11 @@ import { ProjetosController } from './projetos.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegrasModule } from 'src/regras/regras.module';
 import { VariacoesModule } from 'src/variacoes/variacoes.module';
+import { RegrasService } from 'src/regras/regras.service';
 
 @Module({
   controllers: [ProjetosController],
-  providers: [ProjetosService, PrismaService],
-  imports: [RegrasModule, VariacoesModule],
+  providers: [ProjetosService, PrismaService, RegrasService],
+  imports: [VariacoesModule],
 })
 export class ProjetosModule {}
